c3bedd4088a6664bfe774524507d64f374660c07
Aathithya13 <164598758+Aathithya13@users.noreply.github.com>
2025-08-24 21:04:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Aathi_Portfolio/', // 👈 use your repo name here
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
